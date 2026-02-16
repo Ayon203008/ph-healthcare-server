d2898f000f3e6d396a364e1cc2fc3c88fcebdf6e
Ayon203008 <paypal572874@gmail.com>
2026-02-16 21:19:14 +0600

    ZOD added to global error handler

--- src/app/middleware/globalErrorhandler.ts
+++ src/app/middleware/globalErrorhandler.ts
@@ -3,16 +3,40 @@
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../../config/env"
 import status from "http-status";
+import z from "zod";
 
-export const globalErrorHandler=(err:any,req:Request,res:Response,next:NextFunction)=>{
-    
-    const statusCode : number =status.INTERNAL_SERVER_ERROR; // * pnpm add http-status --> install is pacakage
-    const message:string='Internal Server Error'
-    
+interface TErrorSource {
+    path: string,
+    message: string
+}
+
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+
+    // * For zod error handeling
+
+    const errorSource: TErrorSource[] = []
+    let statusCode: number = status.INTERNAL_SERVER_ERROR; // * pnpm add http-status --> install is pacakage
+    let message: string = "Internal Server Error"
+
+    if (err instanceof z.ZodError) {
+        statusCode = status.INTERNAL_SERVER_ERROR;
+        message = "zod valodation error";
+
+        err.issues.forEach(issue => {
+            errorSource.push({
+                path: issue.path.join(" "),
+                message: issue.message
+            })
+        })
+    }
+
     res.status(500).json({
-        success:false,
-        message:"Internal server error",
-        error:err.message
+        success: false,
+        message: "Internal server error",
+        error: err.message,
+        errorSource
     })
 }
+
+
 
